934a01c1fa58a5a83104e3abf432bba0a7fdeb4e
A. Ross Morton <arossco@ntlworld.com>
2026-01-20 18:03:34 +0000

    Initial Commit

---
+++ YouAreAwesome26/YouAreAwesome26App.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome26App.swift
+//  YouAreAwesome26
+//
+//  Created by Ross Morton on 20/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesome26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
